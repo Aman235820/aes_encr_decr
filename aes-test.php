dfc3af07daa092cb06fb49218864a358754627ed
Aman415 <68608077+Aman415@users.noreply.github.com>
2021-12-04 23:12:03 +0530

    aes

---
+++ aes-test.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'aes.php';
+$inputText = "Aman Somani";
+$inputKey = "123456789";
+$blockSize = 256;
+$aes = new AES($inputText, $inputKey, $blockSize);
+$enc = $aes->encrypt();
+$aes->setData($enc);
+$dec=$aes->decrypt();
+echo "After encryption: ".$enc."<br/>";
+echo "After decryption: ".$dec."<br/>";
+
+?>
